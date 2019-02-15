d6f1ecd02a1096dcf9528339fbf1f120bb69b486
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2019-02-15 23:12:24 +0100

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # simple-junit-test
+
+This project is a template to test processes with [camunda-bpm-assert](https://github.com/camunda/camunda-bpm-assert) and [process test coverage](https://github.com/camunda/camunda-process-test-coverage) with JUnit.
+
+Just copy the project to your disk, copy your process into `src/main/resources`, complete the `InMemoryH2Test.java` and run it with `mvn clean test`.
+
+You can inspect the process test coverage unter `target/process-test-coverage` by opening the files in your browser.
---
+++ src/test/java/com/camunda/consulting/InMemoryH2Test.java
@@ -0,0 +1,49 @@
+package com.camunda.consulting;
+
+import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.junit.*;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+public class InMemoryH2Test {
+
+  @ClassRule
+  @Rule
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
+
+  private static final String PROCESS_DEFINITION_KEY = "mySampleProcess";
+
+  static {
+    LogFactory.useSlf4jLogging(); // MyBatis
+  }
+
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
+  }
+
+  /**
+   * Just tests if the process definition is deployable.
+   */
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testParsingAndDeployment() {
+    // nothing is done here, as we just want to check for exceptions during deployment
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testHappyPath() {
+    //ProcessInstance processInstance = runtimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+    // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
+
+  }
+}
